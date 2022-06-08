07dcaf93340c86eb196f15c94dce42e4cacd6bc4
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-06-08 21:41:44 +0600

    add: models

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
+from dwitter.models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline]
 
 
+# admin.site.register(Profile)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 # Remove 'Group' from admin
